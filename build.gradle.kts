ecc3e99de9982665a9ad8757da388b51fdfe703e
K.S. Yim <point1304@gmail.com>
2020-07-13 09:26:04 +0900

    bump up java version from 1.8 to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ subprojects {
     }
 
     configure<JavaPluginConvention> {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     configurations {
