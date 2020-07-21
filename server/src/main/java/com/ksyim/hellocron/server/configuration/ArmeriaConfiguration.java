9b4be3cb86b31008b0f520fdbf92bb214048333f
K.S. Yim <point1304@gmail.com>
2020-07-21 21:15:30 +0900

    add divergence of  between dev and production environment

--- server/src/main/java/com/ksyim/hellocron/server/configuration/ArmeriaConfiguration.java
+++ server/src/main/java/com/ksyim/hellocron/server/configuration/ArmeriaConfiguration.java
@@ -1,8 +1,10 @@
 package com.ksyim.hellocron.server.configuration;
 
 import com.ksyim.hellocron.server.controller.CronController;
+import com.linecorp.armeria.client.ClientFactory;
 import com.linecorp.armeria.client.WebClient;
 import com.linecorp.armeria.client.logging.LoggingClient;
+import com.linecorp.armeria.common.CommonPools;
 import com.linecorp.armeria.common.auth.OAuth2Token;
 import com.linecorp.armeria.common.util.EventLoopGroups;
 import com.linecorp.armeria.server.docs.DocService;
@@ -21,15 +23,15 @@ public class ArmeriaConfiguration {
 
     static final String LINE_MESSAGING_API_TOKEN = requireNonNull(
             System.getenv("LINE_MESSAGING_API_TOKEN"),
-            "environment variable `LINE_MESSAGING_API_TOKEN` is not set");
-
+            "env variable `LINE_MESSAGING_API_TOKEN` is not set");
     static final int PORT = Integer.parseInt(
-            requireNonNull(System.getenv("PORT"), "environment variable `PORT` is not set"));
+            requireNonNull(System.getenv("PORT"), "env variable `PORT` is not set"));
 
     @Bean
-    public ArmeriaServerConfigurator armeriaServerConfigurator() {
+    public ArmeriaServerConfigurator armeriaServerConfigurator(EventLoopGroup loopGroup) {
         return builder -> {
             builder.http(PORT)
+                    .workerGroup(loopGroup, false)
                     .decorator(LoggingService.newDecorator())
                     .accessLogWriter(AccessLogWriter.combined(), false)
                     .serviceUnder("/docs", new DocService());
@@ -38,7 +40,23 @@ public class ArmeriaConfiguration {
 
     @Bean
     public EventLoopGroup getEventLoopWorkerGroup() {
-        return EventLoopGroups.newEventLoopGroup(4);
+        String env = requireNonNull(
+                System.getenv("ARMERIA_ENV"), "env variable `ARMERIA_ENV` is not set");
+
+        if (env.equals("production")) { return CommonPools.workerGroup(); }
+        else if (env.equals("development")) { return EventLoopGroups.newEventLoopGroup(1); }
+        else {
+            throw new RuntimeException(String.format(
+                    "env variable `ARMERIA_ENV` must either be `production` or `development` but " +
+                            "`%s` was given.", env));
+        }
+    }
+
+    @Bean
+    public ClientFactory clientFactory(EventLoopGroup loopGroup) {
+        return ClientFactory.builder()
+                .workerGroup(loopGroup, false)
+                .build();
     }
 
     @Bean
@@ -49,8 +67,9 @@ public class ArmeriaConfiguration {
     }
 
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder("https://api.line.me")
+    public WebClient webClient(ClientFactory factory) {
+        return WebClient.builder("http://localhost:3000")
+                .factory(factory)
                 .decorator(LoggingClient.newDecorator())
                 .auth(OAuth2Token.of(LINE_MESSAGING_API_TOKEN))
                 .build();
--- server/src/main/java/com/ksyim/hellocron/server/configuration/CronConfiguration.java
+++ server/src/main/java/com/ksyim/hellocron/server/configuration/CronConfiguration.java
@@ -4,6 +4,7 @@ import com.ksyim.hellocron.server.cron.CronScheduler;
 import com.ksyim.hellocron.server.cron.DefaultCronScheduler;
 import com.ksyim.hellocron.server.cron.JitterCronScheduler;
 import com.linecorp.armeria.common.CommonPools;
+import io.netty.channel.EventLoopGroup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class CronConfiguration {
 
     @Bean
-    public CronScheduler getCronScheduler() {
-        return new JitterCronScheduler(CommonPools.workerGroup());
+    public CronScheduler cronScheduler(EventLoopGroup loopGroup) {
+        return new DefaultCronScheduler(loopGroup);
     }
 }
